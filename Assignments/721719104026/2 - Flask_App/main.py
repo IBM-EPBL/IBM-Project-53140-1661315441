30b701c8cdb563b379698898cce532e98bdd625e
Ganappriyan <ganappriyanc@gmail.com>
2022-10-23 08:16:09 +0530

    Assignment Updated with IBM DB2 instead of Sqlite

--- Assignments/721719104026/2 - Flask_App/main.py
+++ Assignments/721719104026/2 - Flask_App/main.py
@@ -1,72 +1,120 @@
 from flask import Flask, render_template, request, session, redirect, url_for
-from userdata import DB
+from dbmanager import DB
 
 app = Flask(__name__)
+db = DB()
 
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
-  if 'window' not in session:
-    session['window'] = 'home'
-  return render_template('index.html')
-
+  if 'win' not in session:
+    session['win'] = 'home'
+  return render_template('index.html')  
 
 @app.route('/home')
 def home():
-  session['window'] = 'home'
+  session['win'] = 'home'
   return redirect(url_for('index'))
-
 
 @app.route('/about')
 def about():
-  session['window'] = 'about'
+  session['win'] = 'about'
   return redirect(url_for('index'))
 
+@app.route('/profile')
+def profile():
+  if 'userdb' not in session:
+    session['msg'] = 'Sign in First!'
+    return redirect(url_for('index'))
+  session['win'] = 'profile'
+  return redirect(url_for('index'))
 
 @app.route('/signin')
 def signin():
-  session['tmp'] = session['window']
-  return render_template('signin.html', msg=None)
-
+  if 'userdb' in session:
+    session['msg'] = 'You are already Signed in'
+    return redirect(url_for('index'))
+  if session['win'] !='signin':
+    if session['win'] not in ('signin', 'signup'):
+      session['tmp'] = session['win']
+    session['win'] = 'signin'
+    if 'form' in session:
+      session.pop('form')
+  return redirect(url_for('index'))
 
 @app.route('/signup')
 def signup():
-  return render_template('signup.html', msg=None)
-
-
-@app.route('/handle_form/<t>', methods=['POST'])
-def handle_form(t):
-  db = DB()
-  if request.method == 'POST':
-    
-    if t == 'signin':
-      if db.check_user(request.form['username'], request.form['password']):
-        session['user'] = request.form['username']
-        session['window'] = session['tmp']
-        return redirect(url_for('index'))
-        
-      else:
-        return render_template('signin.html', msg='Invalid username or password')
-        
-    elif t == 'signup':
-      if db.check_username(request.form['username']):
-        db.add(request.form['username'],
-             request.form['firstname'],
-             request.form['lastname'],
-             request.form['email'],
-             request.form['password'])
-        session['user'] = request.form['username']
-        session['window'] = session['tmp']
-        return redirect(url_for('index'))
-      else:
-        return render_template('signup.html', msg='Username already exists')
-
+  if 'userdb' in session:
+    session['msg'] = 'You are already Signed in'
+    return redirect(url_for('index'))
+  if session['win'] !='signup':
+    if session['win'] not in ('signin', 'signup'):
+      session['tmp'] = session['win']
+    session['win'] = 'signup'
+    if 'form' in session:
+      session.pop('form')
+  
+  return redirect(url_for('index'))
 
 @app.route('/signout')
 def signout():
-  session.pop('user')
+  if 'userdb' not in session:
+    session['msg'] = 'You are not signed in'
+    return redirect(url_for('index'))
+  session.pop('userdb')
   return redirect(url_for('index'))
 
+@app.route('/handle_form', methods=['POST'])
+def handle_form():
+  if request.method == 'POST':
+    
+    if session['win'] == 'signin':
+      session['form'] = {'username':request.form['username'],
+                         'password':request.form['password']}
+      if db.check_user(session['form']['username'], session['form']['password']):
+        data = db.get_data(session['form']['username'])
+        session['userdb'] = {'username':data['USERNAME'],
+                             'firstname':data['FIRSTNAME'],
+                             'lastname':data['LASTNAME'],
+                             'email':data['EMAIL'],
+                             'phone':data['PHONE']}
+        session.pop('form')
+        session['win'] = session['tmp']
+        return redirect(url_for('index'))
+      else:
+        session['msg'] = 'Invalid username or password'
+        return redirect(url_for('index'))
+    
+    elif session['win'] == 'signup':
+      session['form'] = {'username':request.form['username'],
+                         'password':request.form['password'],
+                         'firstname':request.form['firstname'],
+                         'lastname':request.form['lastname'],
+                         'email':request.form['email'],
+                         'phone':request.form['phone'],}
+      if db.get_data(session['form']['username']) == None:
+        db.add(session['form']['username'],
+               session['form']['password'],
+               session['form']['firstname'],
+               session['form']['lastname'],
+               session['form']['email'],
+               session['form']['phone'],)
+        data = db.get_data(session['form']['username'])
+        session['userdb'] = {'username':data['USERNAME'],
+                             'firstname':data['FIRSTNAME'],
+                             'lastname':data['LASTNAME'],
+                             'email':data['EMAIL'],
+                             'phone':data['PHONE']}
+        session.pop('form')
+        session['win'] = session['tmp']
+        return redirect(url_for('index'))
+      else:
+        session['msg'] = 'Username already exists'
+        return redirect(url_for('index'))
+    
+    else:
+      session['msg'] = 'Error with Form'
+      return redirect(url_for('index'))
 
 app.config['SECRET_KEY'] = '123456789'
-app.run(debug = True)  
+app.run()
