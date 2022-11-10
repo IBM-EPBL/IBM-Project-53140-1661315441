d74b9c065eaa589b6a82ebd1ae69446ad303d198
Ganappriyan <ganappriyanc@gmail.com>
2022-11-10 11:12:33 +0530

    Flask Backend & UserManagement Module

--- Project Development Phase/Sprint 1/main.py
+++ Project Development Phase/Sprint 1/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from modules.dbmanager import DBManager
+from modules.UserManagement import UserManagement
 
 app = Flask(__name__)
 HOSTNAME = "21fecfd8-47b7-4937-840d-d791d0218660.bs2io90l08kqb1od8lcg.databases.appdomain.cloud"
@@ -7,7 +8,100 @@ PORT = 31864
 SSLServerCertificate = 'DigiCertGlobalRootCA.crt'
 UID = "wxq10827"
 PWD = "ymVaT0kPM3W5j0g5"
-DB = DBManager(HOSTNAME, PORT, SSLServerCertificate, UID, PWD)
+UM = UserManagement(DBManager(HOSTNAME, PORT, SSLServerCertificate, UID, PWD))
+
+# Root URL
+@app.route('/')
+def index():
+  return redirect(url_for('dashboard'))
+
+# Dashboard - Page
+@app.route('/dashboard')
+def dashboard():
+  session['page'] = 'dashboard'
+  return render_template('dashboard.html')
+
+# Store - Page
+@app.route('/store')
+def store():
+  session['page'] = 'store'
+  return render_template('store.html')
+
+# Warehouse - Page
+@app.route('/warehouse')
+def warehouse():
+  session['page'] = 'warehouse'
+  return render_template('warehouse.html')
+
+# User Management - Page
+@app.route('/usermanagement')
+def usermanagement():
+  session['page'] = 'usermanagement'
+  return render_template('usermanagement.html')
+
+# Profile - Page
+@app.route('/profile')
+def profile():
+  if not UM.user:
+    session['msg'] = 'Sign in First!'
+    return redirect(url_for(session['page']))
+  session['page'] = 'profile'
+  return render_template('profile.html')
+
+# Sign In - Page
+@app.route('/signin')
+def signin():
+  if UM.user:
+    session['msg'] = 'You are already Signed in'
+    return redirect(url_for(session['page']))
+  if session['page'] !='signin':
+    if 'form' in session:
+      session.pop('form')
+    session['tmp'] = session['page']
+    session['page'] = 'signin'
+  return render_template('signin.html')
+
+@app.route('/signin/handle_form', methods=['POST'])
+def signin_form():
+  session['form'] = {'username':request.form['username'],
+                     'password':request.form['password']}
+  try:
+    UM.signin(session['form']['username'], session['form']['password'])
+    session['msg'] = 'Signed in successfully'
+    session['page'] = session['tmp']
+  except Exception as e:
+    session['msg'] = str(e)
+    return redirect(url_for(session['page']))
+  session.pop('form')
+  return redirect(url_for(session['page']))
+
+# Sign Out
+@app.route('/signout')
+def signout():
+  try:
+    UM.signout()
+    session['msg'] = 'Signed out successfully'
+  except Exception: session['msg'] = Exception
+  return redirect(url_for(session['page']))
+
+
+# Remove User
+@app.route('/removeuser')
+def removeuser():
+  try:
+    UM.remove_user()
+    session['msg'] = 'User removed successfully'
+  except Exception: session['msg'] = Exception
+  return redirect(url_for(session['page']))
+
+
+app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.context_processor
+def inject_data():
+  return dict(UM=UM)
 
 
 if __name__ == '__main__':
---
+++ Project Development Phase/Sprint 1/modules/UserManagement.py
@@ -0,0 +1,88 @@
+from modules.dbmanager import DBManager
+
+
+class User:
+  def __init__(self, DB, username, password, name=None, role=None, email=None, phone=None, pre=False):
+    self.DB = DB
+    self.USERNAME = username
+    self.PASSWORD = password
+    self.npassword = None
+
+    if all([name, role, email, phone]):  # Sign-up
+      if not pre:
+        DB.add_user(username, password, name, role, email, phone)
+      self.name = name
+      self.role = role
+      self.email = email
+      self.phone = phone
+    else:  # Sign-in
+      self.pull()
+
+  def push(self):
+    self.DB.update_user(self.USERNAME, self.PASSWORD, self.npassword,
+              self.name, self.role, self.email, self.phone)
+    self.npassword = None
+
+  def pull(self):
+    d = self.DB.get_user(self.USERNAME, self.PASSWORD)
+    self.name = d['name']
+    self.role = d['role']
+    self.email = d['email']
+    self.phone = d['phone']
+
+  def remove(self):
+    self.DB.remove_user(self.USERNAME, self.PASSWORD)
+
+
+class UserManagement:
+  def __init__(self, DB):
+    self.DB = DB
+    self.user = None
+
+  def signin(self, username, password):
+    print("UserManagement.signin(): username = %s, password = %s" %
+              (username, password))
+    self.user = User(self.DB, username, password)
+
+  def signout(self):
+    print("UserManagement.signout(): user = %s" % self.user)
+    if self.user:
+      self.user = None
+    else:
+      raise Exception("No user signed in")
+
+  def signup(self, username, password, name, role, email, phone):
+    print("UserManagement.signup(): username = %s, password = %s, name = %s, role = %s, email = %s, phone = %s" % (
+      username, password, name, role, email, phone))
+    self.user = User(self.DB, username, password, name, role, email, phone)
+
+  def update(self, name, role, email, phone):
+    print("UserManagement.update(): name = %s, role = %s, email = %s, phone = %s" % (
+      name, role, email, phone))
+    if not self.user:
+      raise Exception("No user signed in")
+    self.user.name = name
+    self.user.role = role
+    self.user.email = email
+    self.user.phone = phone
+    self.user.push()
+  
+  def update_password(self, password, npassword):
+    if not self.user:
+      raise Exception("No user signed in")
+    if self.user.PASSWORD != password:
+      raise Exception("Incorrect old password")
+    if self.user.PASSWORD == npassword:
+      raise Exception("New password must be different from old password")
+    self.user.npassword = npassword
+    self.user.push()
+
+  def remove_user(self):
+    print("UserManagement.remove_user(): user = %s" % self.user)
+    if not self.user:
+      raise Exception("No user signed in")
+    self.user.remove()
+    self.user = None
+
+  def get_users(self):
+    return [User(self.DB, *d, pre=True) for d in self.DB.get_users()]
