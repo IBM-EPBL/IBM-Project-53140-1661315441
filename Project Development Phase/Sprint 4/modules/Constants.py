e68bad79b77d5ceb920b8a8090f3a00894f96bae
Ganappriyan <ganappriyanc@gmail.com>
2022-11-29 06:54:07 +0530

    Fixed Code Format

--- Project Development Phase/Sprint 4/modules/Constants.py
+++ Project Development Phase/Sprint 4/modules/Constants.py
@@ -1,5 +1,7 @@
 class Constants:
     def __init__(self):
         self.SORTBY = ('id', 'name', 'type', 'price', 'quantity', 'facility')
-        self.USERPRIVILEGES = {'superadmin':-1, 'admin':0, 'user':1, 'visitor':2}
-        self.CATEGORIES = ('uncategorised','grocery','snack','homeappliance','stationary','electronics','other')
+        self.USERPRIVILEGES = {'superadmin': -1,
+                               'admin': 0, 'user': 1, 'visitor': 2}
+        self.CATEGORIES = ('uncategorised', 'grocery', 'snack',
+                           'homeappliance', 'stationary', 'electronics', 'other')
